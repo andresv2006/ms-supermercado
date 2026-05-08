837a0828376faac582c1854fc42abc0e056b67a0
andresv2006 <andr.vargasm@duocuc.cl>
2026-05-08 14:04:44 -0400

    Refactor JwtUtil class for improved structure

--- ms-auth-20260508T172059Z-3-001/ms-auth/src/main/java/com/example/ms_auth/security/JwtUtil.java
+++ ms-auth-20260508T172059Z-3-001/ms-auth/src/main/java/com/example/ms_auth/security/JwtUtil.java
@@ -19,7 +19,7 @@ public class JwtUtil {
         this.key = Keys.hmacShaKeyFor(secret.getBytes());
     }
 
-    // 🔐 GENERAR ACCESS TOKEN
+    
     public String generarToken(String username, String role) {
 
         return Jwts.builder()
@@ -31,7 +31,6 @@ public class JwtUtil {
                 .compact();
     }
 
-    // 🔄 GENERAR REFRESH TOKEN
     public String generarRefreshToken(String username) {
 
         return Jwts.builder()
@@ -43,7 +42,6 @@ public class JwtUtil {
                 .compact();
     }
 
-    // ✅ VALIDAR TOKEN
     public boolean esValido(String token) {
         try {
             Claims claims = getClaims(token);
@@ -53,22 +51,18 @@ public class JwtUtil {
         }
     }
 
-    // 👤 OBTENER USUARIO
     public String obtenerUsuario(String token) {
         return getClaims(token).getSubject();
     }
 
-    // 🔐 OBTENER ROLE
     public String obtenerRole(String token) {
         return getClaims(token).get("role", String.class);
     }
 
-    // 🔄 VALIDAR SI ES REFRESH TOKEN
     public boolean esRefreshToken(String token) {
         return "refresh".equals(getClaims(token).get("type"));
     }
 
-    // 🔍 EXTRAER CLAIMS
     private Claims getClaims(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(key)
