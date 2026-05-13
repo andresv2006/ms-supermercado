cc0ec016c20f769559d1449ee159607ba87a6673
andresv2006 <andr.vargasm@duocuc.cl>
2026-05-12 22:04:15 -0400

    Add files via upload

---
+++ ms-reporte/src/test/java/com/example/ms_reporte/MsReporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms_reporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsReporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
