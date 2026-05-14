d19349879ffeb81d8b0a13782353d653adb4f0c1
andresv2006 <andr.vargasm@duocuc.cl>
2026-05-14 00:51:17 -0400

    crear entidad empleado

---
+++ ms-empleado/src/main/java/com/example/ms_empleado/model/Empleado.java
@@ -0,0 +1,52 @@
+package com.example.ms_empleado.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "empleado")
+public class Empleado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String rut;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String apellido;
+
+    @Column(nullable = false, unique = true)
+    private String correo;
+
+    @Column(nullable = false)
+    private String telefono;
+
+    @Column(nullable = false)
+    private String cargo;
+
+    @Column(nullable = false)
+    private String turno;
+
+    @Column(nullable = false)
+    private BigDecimal sueldo;
+
+    @Column(nullable = false)
+    private Boolean activo;
+}
