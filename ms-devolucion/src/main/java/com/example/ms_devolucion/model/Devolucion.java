a09c041032de5f4f3a5a5c10dcf3ef8b66a3de27
waton123waton <c246326@gmail.com>
2026-05-14 19:54:40 -0400

    devolucion: agregar modelo dto y repositorio

---
+++ ms-devolucion/src/main/java/com/example/ms_devolucion/model/Devolucion.java
@@ -0,0 +1,37 @@
+package com.example.ms_devolucion.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "devolucion")
+public class Devolucion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long pedidoId;
+
+    @Column(nullable = false)
+    private Long pagoId;
+
+    @Column(nullable = false)
+    private String motivo;
+
+    @Column(nullable = false)
+    private String estado;
+}
