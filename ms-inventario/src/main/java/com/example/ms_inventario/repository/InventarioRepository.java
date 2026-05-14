dfbf6ae72534ce6ccec58a07479faa30190bb7f7
waton123waton <c246326@gmail.com>
2026-05-14 18:39:14 -0400

    inventario: agregar busqueda por producto

--- ms-inventario/src/main/java/com/example/ms_inventario/repository/InventarioRepository.java
+++ ms-inventario/src/main/java/com/example/ms_inventario/repository/InventarioRepository.java
@@ -1,5 +1,7 @@
 package com.example.ms_inventario.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,6 @@ import com.example.ms_inventario.model.Inventario;
 
 @Repository
 public interface InventarioRepository extends JpaRepository<Inventario, Long> {
+
+    Optional<Inventario> findByProductoId(Long productoId);
 }
