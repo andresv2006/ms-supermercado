129f71affd951c51d6ff249afd88d9a077d1c427
andresv2006 <andr.vargasm@duocuc.cl>
2026-06-16 01:37:57 -0400

    crear servidor Eureka

---
+++ ms-eureka/src/test/java/com/example/ms_eureka/MsEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms_eureka;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+class MsEurekaApplicationTests {
+
+	@Test
+	void applicationClassExists() {
+		assertNotNull(MsEurekaApplication.class);
+	}
+}
