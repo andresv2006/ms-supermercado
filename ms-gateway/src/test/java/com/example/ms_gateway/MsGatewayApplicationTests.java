ce4ec57d55b019d17116b9e5ac99cd532c812b4c
andresv2006 <andr.vargasm@duocuc.cl>
2026-06-16 22:46:44 -0400

    api gateway

---
+++ ms-gateway/src/test/java/com/example/ms_gateway/MsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms_gateway;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+class MsGatewayApplicationTests {
+
+	@Test
+	void applicationClassExists() {
+		assertNotNull(MsGatewayApplication.class);
+	}
+}
